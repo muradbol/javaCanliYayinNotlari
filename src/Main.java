98f5cf0ea2ae128dfae591941489d732afe20b83
muradbol <murat.bolelli@ogr.sakarya.edu.tr>
2022-09-15 19:32:44 +0300

    proje ismi değiştirildi.

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Selam dünya");
-
         System.out.println("Lütfen doların dunki değerini girin");
         //Scanner kullanıcıdan değer almak için kullandığımız parametre
-
         Scanner alinandeger=new Scanner(System.in);
         double dolarDun= alinandeger.nextDouble();
         System.out.println("Lütfen doların bugünün değerini girin");
         double dolarBugun= alinandeger.nextDouble();
         //integer kullanımları
-       // double dolarDun=17.64;
-       // double dolarBugun=17.60;
         if (dolarDun==dolarBugun){
             System.out.println("Dolar dünki kurla aynı");
         }else if (dolarDun>dolarBugun){
