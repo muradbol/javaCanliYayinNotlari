f5b76895858db2a9c04b1de752a0835d831572f8
muradbol <murat.bolelli@ogr.sakarya.edu.tr>
2022-09-15 19:29:10 +0300

    fazlalıklar giderildi yorum satırları eklendi

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Selam dünya");
 
-
-        //değişken stringler
-        String ortaMetin ="İlginizi Çekebilir";
-        String altMetin = "Vade Süresi";
         System.out.println("Lütfen doların dunki değerini girin");
+        //Scanner kullanıcıdan değer almak için kullandığımız parametre
+
         Scanner alinandeger=new Scanner(System.in);
         double dolarDun= alinandeger.nextDouble();
         System.out.println("Lütfen doların bugünün değerini girin");
         double dolarBugun= alinandeger.nextDouble();
-
-        System.out.println("deneme");
-
-        System.out.println(ortaMetin);
         //integer kullanımları
-
-        int vade = 12;
        // double dolarDun=17.64;
        // double dolarBugun=17.60;
         if (dolarDun==dolarBugun){
